73e60abb59b8038abaed61fbb5c9c2a5bab25ba6
Donielle Berg <donielle.berg@gmail.com>
2017-10-02 19:26:47 -0700

    Add bad a11y example Twitter post

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom'
 
 import './App.css';
+
 import BadA11yExample from './BadA11yExample';
 import GoodA11yExample from './GoodA11yExample';
+import IconSettings from 'design-system-react/components/iconSettings';
 
 class App extends Component {
   render() {
     return (
 			<Router>
-				<div className="App">
-					<ul>
-						<li><Link to="/">Home</Link></li>
-						<li><Link to="/badA11y">Bad A11y Example</Link></li>
-						<li><Link to="/goodA11y">Good A11y Example</Link></li>
+				<IconSettings iconPath="./assets/icons">
+				<div className="ht-full df">
+					<ul className="nav pvm slds-size_1-of-5 slds-border_right bg-navy text-aqua">
+						<li className="f3 caps phm pvs text-aqua slds-border_bottom fw-bold">A11y Demo</li>
+						<li className="f4"><NavLink className="db phm pvs" to="/badA11y">Bad Example</NavLink></li>
+						<li className="f4"><NavLink className="db phm pvs" to="/goodA11y">Good Example</NavLink></li>
 					</ul>
-    			<Route path="/badA11y" component={BadA11yExample} />
-					<Route path="/goodA11y" component={GoodA11yExample} />
+					<div className="pam slds-size_4-of-5">
+						<Route path="/badA11y" component={BadA11yExample} />
+						<Route path="/goodA11y" component={GoodA11yExample} />
+					</div>
 				</div>
+				</IconSettings>
 			</Router>
     );
   }
--- src/BadA11yExample.js
+++ src/BadA11yExample.js
@@ -1,12 +1,62 @@
 import React, { Component } from 'react';
-import salesforceLogo from './assets/DF14_Alternate_Cloud_4C.jpg'
+
+import MediaObject from 'design-system-react/components/media-object';
+import Icon from 'design-system-react/components/icon';
+import MenuDropdown from 'design-system-react/components/menu-dropdown';
+import DropdownTrigger from 'design-system-react/components/menu-dropdown/button-trigger';
+import Button from 'design-system-react/components/button/';
 
 class BadA11yExample extends Component {
   render() {
     return (
-      <div style={{ marginTop: '200px', display: 'flex', alignItems: 'center'}}>
-					<img src={salesforceLogo} alt="" width="408" height="206" />
-					<span style={{ fontSize: '3rem', marginLeft: '-2rem' }}>Salesforce</span>
+      <div>
+				<div className="slds-size_1-of-4 center">
+					<img src="./assets/images/DLFUfUxXcAAUQOs.png" className="db wi-full" />
+					<div>
+						<header className="pts">
+							<MediaObject
+								body={
+									<div className="df df-justify">
+										<p>
+											Brad Paisley
+											<p className="db f5 text-lt-gray">@BradPaisley</p>
+										</p>
+										<p className="f5 text-lt-gray">
+											Oct 1
+										</p>
+									</div>
+								}
+								figure={<Icon category="standard" name="user" size="medium" />}
+								verticalCenter
+							/>
+						</header>
+						<div className="df df-justify">
+							<p className="f5">
+								Um, way to expand your business portfolio <a href="javascript:void(0)">@blakeshelton</a>. Big congratulations. Way to diversify.
+							</p>
+							<MenuDropdown
+								nubbinPosition="top left"
+								onSelect={(value) => { console.log('selected: ', value); }}
+								options={[
+									{ label: 'Copy link to Tweet', value: 'A0' }
+								]}
+							>
+								<DropdownTrigger>
+									<Button
+										iconName="switch"
+										iconSize="small"
+										variant="icon"
+									/>
+								</DropdownTrigger>
+							</MenuDropdown>
+						</div>
+						<footer className="pts">
+							<button><i className="text-lt-gray mrx fa fa-comment-o" /></button>
+							<button><i className="text-lt-gray mrx fa fa-retweet" /></button>
+							<button><i className="text-lt-gray mrx fa fa-heart-o" /></button>
+						</footer>
+					</div>
+				</div>
       </div>
     );
   }
